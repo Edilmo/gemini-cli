22ef3151a9e11665121abd221d840bb0f0f74fcb
Edilmo Palencia <edilmo@gmail.com>
2025-07-07 13:30:47 -0700

    Fix lint error

--- integration-tests/memory-extension.test.js
+++ integration-tests/memory-extension.test.js
@@ -11,7 +11,6 @@ import { spawn } from 'child_process';
 import { join } from 'path';
 import { fileURLToPath } from 'url';
 import { writeFileSync, mkdirSync, cpSync } from 'fs';
-import esbuild from 'esbuild';
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 const projectRoot = join(__dirname, '..');
